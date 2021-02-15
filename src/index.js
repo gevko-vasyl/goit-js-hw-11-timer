87a5209b982948c790017359dd692bc68b5c5417
Vasyl Hevko <hevko.vasyl.me@gmail.com>
2021-02-15 20:46:45 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,19 +1,50 @@
 import './styles.css';
 
-const daysRef = document.querySelector('[data-value="days"]');
-const hoursRef = document.querySelector('[data-value="hours"]');
-const minsRef = document.querySelector('[data-value="mins"]');
-const secsRef = document.querySelector('[data-value="secs"]');
+// const daysRef = document.querySelector('[data-value="days"]');
+// const hoursRef = document.querySelector('[data-value="hours"]');
+// const minsRef = document.querySelector('[data-value="mins"]');
+// const secsRef = document.querySelector('[data-value="secs"]');
 
-daysRef.textContent = '00';
-hoursRef.textContent = '00';
-minsRef.textContent = '00';
-secsRef.textContent = '00';
+// daysRef.textContent = '00';
+// hoursRef.textContent = '00';
+// minsRef.textContent = '00';
+// secsRef.textContent = '00';
 
 class CountdownTimer{
-    constructor({ selector, targetDate } ) {
+
+    constructor({ selector, targetDate }) {
+
+        this.template = `<div class="field">
+    <span class="value" data-value="days">11</span>
+    <span class="label">Days</span>
+  </div>
+
+  <div class="field">
+    <span class="value" data-value="hours">11</span>
+    <span class="label">Hours</span>
+  </div>
+
+  <div class="field">
+    <span class="value" data-value="mins">11</span>
+    <span class="label">Minutes</span>
+  </div>
+
+  <div class="field">
+    <span class="value" data-value="secs">11</span>
+    <span class="label">Seconds</span>
+  </div>`;
+        
         this.selector = selector;
+        this.root = document.querySelector(this.selector)
         this.targetDate = targetDate;
+        this.root.insertAdjacentHTML('beforeend', this.template);
+        this.refs = {
+            days : this.root.querySelector('[data-value="days"]'),
+            hours : this.root.querySelector('[data-value="hours"]'),
+            mins : this.root.querySelector('[data-value="mins"]'),
+            secs : this.root.querySelector('[data-value="secs"]'),
+        }
+        this.startCountdown();
     }
 
     startCountdown() {
@@ -38,22 +69,22 @@ class CountdownTimer{
         const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
         const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-        daysRef.textContent = `${days}`;
-        hoursRef.textContent = `${hours}`;
-        minsRef.textContent = `${mins}`;
-        secsRef.textContent = `${secs}`;
+        this.refs.days.textContent = `${days}`;
+        this.refs.hours.textContent = `${hours}`;
+        this.refs.mins.textContent = `${mins}`;
+        this.refs.secs.textContent = `${secs}`;
 
         if (days <= 0) {
-           daysRef.textContent = '00'; 
+           this.refs.days.textContent = '00'; 
         };
         if (hours <= 0) {
-           hoursRef.textContent = '00'; 
+           this.refs.hours.textContent = '00'; 
         };
         if (mins <= 0) {
-           minsRef.textContent = '00'; 
+           this.refs.mins.textContent = '00'; 
         };
         if (secs <= 0) {
-           secsRef.textContent = '00'; 
+           this.refs.secs.textContent = '00'; 
         };
 
     }
@@ -65,4 +96,14 @@ const timer = new CountdownTimer({
   targetDate: new Date('Jul 17, 2021'),
 });
 
-timer.startCountdown();
+
+const timer2= new CountdownTimer({
+  selector: '#timer-2',
+  targetDate: new Date('Feb 17, 2021'),
+});
+
+const timer3= new CountdownTimer({
+  selector: '#timer-3',
+  targetDate: new Date('Feb 28, 2021'),
+});
+
